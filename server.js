d53e535b4a3a32c86bab3e23569222afe1a7322f
Abdelrahman-Mohamed <abdelrahman.mohamed7337@gmail.com>
2024-11-02 13:03:21 +0200

    Intall node packages

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.static('website'));
 
 // Setup Server
-const port = 3000;
+const port = 8000;
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 });
